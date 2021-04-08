59cf0819bd1558bfa2ef288b8cf47be36b7c628c
susan <susannajbrown@yahoo.com>
2021-04-08 18:51:07 -0400

    Updates

---
+++ precache-manifest.8a41fa5d1f107ed832e8c43dc5eb41f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03706cb7279cb8faa73a8e268870d03d",
+    "url": "/LoveChain/index.html"
+  },
+  {
+    "revision": "94533e8cb2ee53bc48c2",
+    "url": "/LoveChain/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "a6082af1c437fdb76d98",
+    "url": "/LoveChain/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "94533e8cb2ee53bc48c2",
+    "url": "/LoveChain/static/js/2.b0212769.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/LoveChain/static/js/2.b0212769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6082af1c437fdb76d98",
+    "url": "/LoveChain/static/js/main.814ab5ac.chunk.js"
+  },
+  {
+    "revision": "4c67df13f6cfc080ff61",
+    "url": "/LoveChain/static/js/runtime-main.f52e79a1.js"
+  },
+  {
+    "revision": "a55281dde41f56cf8bd030b39e1e3755",
+    "url": "/LoveChain/static/media/InfoIconBlue.a55281dd.jpg"
+  },
+  {
+    "revision": "07175b8fc306a0628ddcf4a1c4d890ac",
+    "url": "/LoveChain/static/media/Love.07175b8f.png"
+  }
+]);
